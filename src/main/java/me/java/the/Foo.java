02c7a78f238527e6e651a517d126008f554a804b
JinoRino <adg0609@naver.com>
2021-08-20 13:42:24 +0900

    기본 메소드와 스태틱 메소드

--- src/main/java/me/java/the/Foo.java
+++ src/main/java/me/java/the/Foo.java
@@ -1,33 +1,48 @@
 package me.java.the;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 import java.util.function.*;
 
 public class Foo {
     public static void main(String[] args) {
-        // 메소드 래퍼런스
-        UnaryOperator<String> hi = (s) -> "hi" + s;
+
+        List<String> names = new ArrayList<>();
+        names.add("glidong");
+        names.add("suhyung");
+        names.add("jino");
+        names.add("illihe");
+
+        // 출력 1번째(forEach)
+        System.out.println("==========1");
+        names.forEach((s) -> {
+            System.out.println(s);
+        });
+        // 출력 2번째(forEach 간결)
+        System.out.println("==========2");
+        names.forEach(System.out::println);
+
+        // 출력 3번째 (Spliterator)
+        System.out.println("==========3");
+        Spliterator<String> spliterator = names.spliterator();
+        while ( spliterator.tryAdvance(System.out::println));
+
+        // 출력 4번째 (removeIf)
+        System.out.println("==========4");
+        names.removeIf(s -> s.startsWith("j"));
+        names.forEach(System.out::println);
+
+        // 출력 5번째 (Comparator)
+        System.out.println("==========5");
+        names.sort(String::compareToIgnoreCase);
+        names.forEach(System.out::println);
 
 
-        // 스태틱 메소드 참조, (타입::스태틱)
-        UnaryOperator<String> hiToMethodRefer = Greeting::hi;
 
-        // 특정 객체의 인스턴스 메소드 참조
-        Greeting greeting = new Greeting();
-        UnaryOperator<String> helloToMethodRefer = greeting::hello;
+        // static 메소드 및 default 메소드는 Notion 참조
+        Comparator<String> compareToIgnoreCase = String::compareToIgnoreCase;
+        names.sort(compareToIgnoreCase.reversed());
 
-        // 생성자 참조
-        Supplier<Greeting> newGreeting = Greeting::new;
 
-        // 매개변수가 있는 생성자 참조
-        // 담는 변수가 다르기 때문에 위 와는 다른 생성자를 사용.
-        Function<String, Greeting> jinhoGreeting = Greeting::new;
-
-        // 임의 객체의 인스턴스 메소드 참조
-        String[] names = {"3","2","4"};
-        Arrays.sort(names, String::compareToIgnoreCase);
-        System.out.println(Arrays.toString(names));
 
 
     }
